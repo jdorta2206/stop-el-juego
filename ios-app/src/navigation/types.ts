d6046402e70595f7a72e9887e12f6c5a8068ebaf
jaime <75831652+jdorta2206@users.noreply.github.com>
2026-09-02 10:03:48 +0200

    add iOS profile route

--- ios-app/src/navigation/types.ts
+++ ios-app/src/navigation/types.ts
@@ -3,6 +3,7 @@ import type { NativeGameConfig } from '../game/gameConfig';
 export type RootStackParamList = {
   Login: undefined;
   Home: undefined;
+  Profile: undefined;
   GameSetup: undefined;
   Game: { config?: NativeGameConfig };
   Multiplayer: undefined;
