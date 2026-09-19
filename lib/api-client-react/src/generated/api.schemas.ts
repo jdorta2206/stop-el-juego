0406a0f269ea9b6b2d62e29fd4488b23f1058023
jaime <75831652+jdorta2206@users.noreply.github.com>
2026-09-19 03:49:37 +0200

    chore(api): tipar foto al crear sala

--- lib/api-client-react/src/generated/api.schemas.ts
+++ lib/api-client-react/src/generated/api.schemas.ts
@@ -176,6 +176,7 @@ export interface CreateRoomRequest {
   hostId: string;
   hostName: string;
   avatarColor?: string;
+  picture?: string | null;
   maxRounds?: number;
   language?: string;
   loginMethod?: string | null;
